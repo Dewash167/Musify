51044a77cc12f23a3bfb7b31c368b79a38585312
Dewash167 <dewashishwankhede@gmail.com>
2020-07-03 23:37:43 +0530

    added playlist

--- app.js
+++ app.js
@@ -1,37 +1,243 @@
-require('dotenv').config()
-var express = require("express");
-const https = require('https');
-var bodyParser = require("body-parser");
-var SpotifyWebApi = require('spotify-web-api-node');
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+var request = require('request'); // "Request" library
+
+var client_id = '8e673483a1d3493b9926f6f8ee3ab1c5'; // Your client id
+var client_secret = '5589eabe4c9245c4b9f6bc0a4db40afc'; // Your secret
+// your application requests authorization
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+
+
+
+
 const app = express();
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Set necessary parts of the credentials on the constructor
-var spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENTID,
-  clientSecret:process.env.CLIENTSECRET
 
-});
-  console.log(process.env.CLIENTID);
-spotifyApi.setAccessToken('BQAc_CWjPEI5ElDFiUNmCl7JQMqUuZJrHVrH__xUBh23LPsNF0_RlFvPotxH0QgWFEv_toWqoc1cfskgd8q74qZi4zSYBkoDD6r_xK_cQ_s8EE1Dix-3JDpAzg0JlYzvwW28LJroEVRJUmnKQA3vYM-3sOnIhnq450E0Vv9_qpVY9sc');
-spotifyApi.getArtistAlbums('1HY2Jd0NmPuamShAr6KMms').then(
-  function(data) {
-    console.log('Artist albums', data.body);
-  },
-  function(err) {
-    console.error(err);
-  }
-);
-let port = 3000;
 
+
+
+app.get("/",function(req,res) {
+
+res.render("start");
+
+
+  });
+
+
+
+
+
+app.post("/",function(req,res) {
+
+  res.render("dashboard");
+
+
+  });
+
+
+
+  app.post("/playlist",function(req,res) {
+    var playlist_id = req.body.playlist;
+
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        // use the access token to access the Spotify Web API
+        var token = body.access_token;
+      //  console.log(token);
+        var options = {
+           url : 'https://api.spotify.com/v1/playlists/'+playlist_id,
+
+        //  url: 'https://api.spotify.com/v1/users/jmperezperez',
+          headers: {
+            'Authorization': 'Bearer ' + token
+          },
+          json: true
+        };
+        request.get(options, function(error, response, body) {
+        //  res.send(body);
+          var name=body.name;
+          var description=body.description;
+          var imgurl=body.images[0].url;
+          var id=body.id;
+          var ownername=body.owner.display_name;
+          var spotifyurl=body.uri;
+          var followers=body.followers.total;
+          var collabarative = body.collaborative;
+          var type= body.type;
+          var snapshopid = body.snapshot_id;
+           res.render('playlist', { name : name,description:description});
+
+        });
+      }
+    });
+
+
+
+  });
+
+
+
+
+    app.post("/artist",function(req,res) {
+      var artistid = req.body.artist;
+
+      request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+          // use the access token to access the Spotify Web API
+          var token = body.access_token;
+        //  console.log(token);
+          var options = {
+             url : 'https://api.spotify.com/v1/playlists/'+artistid,
+
+          //  url: 'https://api.spotify.com/v1/users/jmperezperez',
+            headers: {
+              'Authorization': 'Bearer ' + token
+            },
+            json: true
+          };
+          request.get(options, function(error, response, body) {
+           res.send(body);
+
+
+          });
+        }
+      });
+
+
+
+    });
+
+      app.post("/tracks",function(req,res) {
+        var track = req.body.track;
+
+
+        request.post(authOptions, function(error, response, body) {
+          if (!error && response.statusCode === 200) {
+
+            // use the access token to access the Spotify Web API
+            var token = body.access_token;
+          //  console.log(token);
+            var options = {
+               url : 'https://api.spotify.com/v1/tracks/'+track,
+
+            //  url: 'https://api.spotify.com/v1/users/jmperezperez',
+              headers: {
+                'Authorization': 'Bearer ' + token
+              },
+              json: true
+            };
+            request.get(options, function(error, response, body) {
+            // res.send(body);
+//  console.log(options.url);
+
+            });
+          }
+        });
+
+
+
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  console.log(userid);
+
+  //
+  // request.post(authOptions, function(error, response, body) {
+  //   if (!error && response.statusCode === 200) {
+  //
+  //     // use the access token to access the Spotify Web API
+  //     var token = body.access_token;
+  //   //  console.log(token);
+  //     var options = {
+  //        url : 'https://api.spotify.com/v1/users/'+userid,
+  //
+  //     //  url: 'https://api.spotify.com/v1/users/jmperezperez',
+  //       headers: {
+  //         'Authorization': 'Bearer ' + token
+  //       },
+  //       json: true
+  //     };
+  //     request.get(options, function(error, response, body) {
+  //       var data = JSON.parse(body);
+  //
+  //
+  //     });
+  //   }
+  // });
+  //
+//
+//
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
 app.listen(port, function() {
-  console.log("Server started on port!");
+  console.log("Server started on port 3000!");
 });
